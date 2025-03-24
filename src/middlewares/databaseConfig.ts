dc4daf51ff7c763b5b638c543e3843864c1c6f95
Ankit <urstrulyankit@protonmail.com>
2025-03-24 16:17:18 +0530

    env file updated

--- src/middlewares/databaseConfig.ts
+++ src/middlewares/databaseConfig.ts
@@ -1,9 +1,17 @@
 import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
 
-export const sequelize = new Sequelize("todo_db", "ags", "ags@123", {
-    host: "localhost",
-    dialect: "mysql",
-});
+dotenv.config({ path: `.env.${process.env.NODE_ENV || "development"}` });
+
+export const sequelize = new Sequelize(
+    process.env.DB_NAME as string,
+    process.env.DB_USER as string,
+    process.env.DB_PASSWORD as string,
+    {
+        host: process.env.DB_HOST,
+        dialect: "mysql",
+    }
+);
 
 export const connectDB = async () => {
     try {
