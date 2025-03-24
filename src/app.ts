9cbc5b19f02f6a82cb6e15b556f97f982274c864
Ankit <urstrulyankit@protonmail.com>
2025-03-24 11:53:33 +0530

    profiling using profiler uning built in profiler and dotenv

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,35 @@
-import express from "express";
 import cors from "cors";
-import todoRoutes from "./routes/todoRoutes";
-import {setupSwagger} from "./middlewares/swaggerConfig";
-import {connectDB, sequelize} from "./middlewares/databaseConfig";
+import dotenv from "dotenv";
+import express from "express";
+import { connectDB, sequelize } from "./middlewares/databaseConfig";
 import logger from "./middlewares/LoggerConfig";
+import { setupSwagger } from "./middlewares/swaggerConfig";
 import authRoute from "./routes/authRoute";
 import postRoutes from "./routes/postRoutes";
+import todoRoutes from "./routes/todoRoutes";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 app.use("/api", todoRoutes);
-app.use("/auth", authRoute)
-app.use('/api', postRoutes);
-setupSwagger(app);
+app.use("/auth", authRoute);
+app.use("/api", postRoutes);
+
+if (process.env.NODE_ENV === "development") {
+    setupSwagger(app);
+    logger.info("Running in development mode");
+} else {
+    logger.info("Running in production mode");
+}
+
+const envFile = `.env.${process.env.NODE_ENV}`;
+dotenv.config({ path: envFile });
 
 const startServer = async () => {
     await connectDB();
     await sequelize.sync({ force: false });
-    app.listen(5000, () => logger.info("Server running on port 5000"));
+    app.listen(5000, () => logger.info(`Server running on port 5000 in ${process.env.NODE_ENV} mode`));
 };
 
 startServer().then(r => console.log("Server started successfully", r));
